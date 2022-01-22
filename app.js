0b972d7b5706c8517cf4b78a1a2775b768c8d535
brodydishman <97564041+brodydishman@users.noreply.github.com>
2022-01-21 21:07:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+//Writing for strings
+
+// let name = "Brody";
+// let l_name = "Dishman";
+
+// User Input
+// let name = prompt("What is your first name?");
+// let l_name = prompt("What is your last name?");
+
+
+// let f_name = name+" "+l_name;
+
+//Writing for numbers
+
+let untaxed_bill = Number(prompt("What was your bill?"));
+
+let tax = untaxed_bill*0.07
+let taxed_bill = untaxed_bill+tax
+let tip = taxed_bill*0.05
+let final_bill = taxed_bill+tip
+//Writing for the strings
+document.body.innerHTML=final_bill;
+// document.body.innerHTML=f_name;
+
+//Writing for numbers
+
+
